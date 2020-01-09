dee6d1a5f16855e37316707571a5ba5148077c49
Bryce Flory <bflory@go-yamamoto.com>
2020-01-09 16:36:49 -0600

    lightbox added, TODO: pass image meta to lightbox

--- src/blocks/Gallery/CoreGalleryBlock.data.js
+++ src/blocks/Gallery/CoreGalleryBlock.data.js
@@ -8,8 +8,22 @@ module.exports = () => {
         columns
         imageCrop
         images
+        ids
       }
-      originalContent
+      galleryImages {
+        nodes {
+          mediaItemId
+          mediaDetails {
+            meta {
+              aperture
+              camera
+              focalLength
+              iso
+              shutterSpeed
+            }
+          }
+        }
+      }
     }
   `
 }
